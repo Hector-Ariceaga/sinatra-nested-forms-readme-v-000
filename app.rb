b02b8832fcc8ef62945d9b7dd17da31528581fdc
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-24 23:16:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
   get '/' do
    erb :new 
   end
+  
+  post '/student' do
+    @student = Student.new(params[:student])
+  end 
    
 end
